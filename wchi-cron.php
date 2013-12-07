30a84aea89b095d30a0926df7cf7213d91a08735
Graham Christensen <graham@grahamc.com>
2013-12-06 20:48:27 -0600

    Calculate rows in the cron DB.

--- wchi-cron.php
+++ wchi-cron.php
@@ -16,6 +16,24 @@ function wchi_cron_write($value, $old_value)
     update_option('wchi-cron-version', $value['version']);
     unset($value['version']);
   }
+
+  wchi_transaction_begin();
+
+  if (get_option('wpchi-cron-overthrown') == false) {
+    # For the first import, set the old value to an empty array
+    # so all entries are imported
+    $old_value = array();
+  }
+
+  $currentValues = calculate_rows($old_value);
+  $newValues = calculate_rows($value);
+
+  var_dump($currentValues);
+  var_dump($newValues);
+
+
+  wchi_transaction_end();
+
   var_export($value);
   exit(1);
 
@@ -35,5 +53,33 @@ function pre_option_cron()
   var_dump($return);
 
   die('wat');
+}
+
+function calculate_rows($value)
+{
+  $rows = array();
+  foreach ($value as $timestamp => $contents) {
+    foreach ($contents as $name => $job) {
+      $rows[] = array(
+        'timestamp' => $timestamp,
+        'name' => $name,
+        'job' => $job
+      );
+    }
+  }
+
+  return $rows;
+}
+
+function wchi_transaction_begin()
+{
+  global $wpdb;
+  $wpdb->query('START TRANSACTION;');
+}
+
+function wchi_transaction_end()
+{
+  global $wpdb;
+  $wpdb->query('COMMIT;');
 }
 
