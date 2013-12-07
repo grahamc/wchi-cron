968ee35376b021229fba17746a7054a9cf90a555
Graham Christensen <graham@grahamc.com>
2013-12-06 20:10:43 -0600

    Add head matter.

---
+++ wchi-cron.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WChi Cron
+ * Plugin URI: https://github.com/grahamc/wchi-cron
+ * Version: 0.1.0
+ * Author: Graham Christensen
+ * Description: Make cron more efficient.
+ */
+
